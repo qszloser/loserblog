f928d6029855c6909e196aa35afc5cedd8385066
qszloser <2961914151@qq.com>
2020-12-24 09:51:39 +0800

    增加项目

--- blog/settings.py
+++ blog/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pyblog',
-        'USER': 'qszloser',
-        'PASSWORD': '123456',
+        'USER': 'root',
+        'PASSWORD': 'Qsz123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
